68dc98b785f810e2b2c5a295e9c85057a62c7a1f
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-11-27 18:28:06 -0500

    contents

---
+++ content/contentTables.js
@@ -0,0 +1,42 @@
+export const formulas = [
+  {
+    id: "1",
+    title: "Potencia Eléctrica",
+    formula: "$P = I^2 \\cdot R$",
+    formula_ios: "P = I^2 · R",
+    description:
+      "Donde:\nP = Potencia en Watts (W)\nI = Corriente en Amperios (A)\nR = Resistencia en Ohms (Ω)",
+  },
+  {
+    id: "2",
+    title: "Cálculo de Potencia",
+    formula: "$P = V \\cdot I$",
+    formula_ios: "P = V · I",
+    description:
+      "Donde:\nP = Potencia en Watts (W)\nV = Voltaje en Voltios (V)\nI = Corriente en Amperios (A)",
+  },
+  {
+    id: "3",
+    title: "Cálculo de Voltaje",
+    formula: "$V = \\frac{P}{I}$",
+    formula_ios: "V = P / I",
+    description:
+      "Donde:\nV = Voltaje en Voltios (V)\nP = Potencia en Watts (W)\nI = Corriente en Amperios (A)",
+  },
+  {
+    id: "4",
+    title: "Cálculo de Corriente",
+    formula: "$I = \\frac{P}{V}$",
+    formula_ios: "I = P / V",
+    description:
+      "Donde:\nI = Corriente en Amperios (A)\nP = Potencia en Watts (W)\nV = Voltaje en Voltios (V)",
+  },
+  {
+    id: "5",
+    title: "Cálculo de Energía Eléctrica",
+    formula: "$E = P \\cdot t$",
+    formula_ios: "E = P ⋅ t",
+    description:
+      "Donde:\nE = Energía en Watt-horas (Wh) o Kilowatt-horas (kWh)\nP = Potencia en Watts (W) o Kilowatts (kW)\nt = Tiempo en horas (h)",
+  },
+];
